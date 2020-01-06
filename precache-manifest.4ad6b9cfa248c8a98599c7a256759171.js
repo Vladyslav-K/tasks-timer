d14d0734e49e8d16ad6b13d4c57f66f506784206
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-01-06 03:01:58 +0200

    Updates

---
+++ precache-manifest.4ad6b9cfa248c8a98599c7a256759171.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2752944df5a2e6092bc1c55870e9a0",
+    "url": "/task-timer/index.html"
+  },
+  {
+    "revision": "eaf41cf1f3d66d3c02ac",
+    "url": "/task-timer/static/js/2.bc378b71.chunk.js"
+  },
+  {
+    "revision": "e7df5ec6c9986765193824c7f118c284",
+    "url": "/task-timer/static/js/2.bc378b71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "670a3a00d2160ffb044d",
+    "url": "/task-timer/static/js/main.b39867cc.chunk.js"
+  },
+  {
+    "revision": "a0fa89f93525e12885d7",
+    "url": "/task-timer/static/js/runtime-main.5ee2e058.js"
+  }
+]);
