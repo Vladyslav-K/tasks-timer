86c9dd4f8c0d14303e5ab14356630ab7263f0419
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-01-15 04:44:49 +0200

    feat: add redux devtools to the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { compose } from "redux";
 import ReactDOM from "react-dom";
 
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import reducers from "./domain/reducers";
 import { Provider } from "react-redux";
-import rootSaga from "./sagas"
+import rootSaga from "./sagas";
 
 import App from "./pages/App";
 import * as serviceWorker from "./serviceWorker";
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
